b6695232a1e125169c2950979efd3f031e1ee3f4
tiffanykawamura <tkawamura02@gmail.com>
2023-07-13 12:33:59 -0700

    update to do list

--- script.js
+++ script.js
@@ -1,33 +1,51 @@
-var text = document.getElementById('input');
-var button = document.getElementById('button');
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
 
-button.addEventListener("click", function() {
-  var str = text.value;
-  text.value = '';
-
-  let list = document.querySelector('#list');
-  let newLI = document.createElement('li');
-  newLI.innerHTML = str;
-  list.appendChild(newLI);
-});
-
-//FIXME: when user clicks enter in textbox trigger button clicked function
-text.addEventListener("keyup", function (event) {
-  if (event.key === 'Enter' || event.keyCode === 13) {
-    var str = text.value;
-    text.value = '';
-
-    let list = document.querySelector('#list');
-    let newLI = document.createElement('li');
-    newLI.innerHTML = str;
-    list.appendChild(newLI);
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
   }
-});
+}
 
-function remove(el) {
-    let list = document.querySelector('#list');
-  list.addEventListener('click', function handleClick(event) {
-      event.target.style.setProperty("text-decoration", "line-through");
-      setTimeout(function(){event.target.remove();},1000);
-  });
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+function add() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("input").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("input").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
 }
